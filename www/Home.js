aae011424d61a6b7a4534b5b095ac0133ef880f9
Antonino Ferrarotti <antoninoferrarotti26@gmail.com>
2025-02-15 17:31:59 +0100

    prova android

---
+++ www/Home.js
@@ -0,0 +1,40 @@
+const startMatchButton = document.getElementById("start-match");
+
+startMatchButton.addEventListener("click", () => {
+  // Raccogli i dati inseriti dall'utente
+  const nameMatch = document
+    .getElementById("nameMatch")
+    .value.trim()
+    .toUpperCase();
+  const nameP1 = document.getElementById("nameP1").value.trim().toUpperCase();
+  const nameP2 = document.getElementById("nameP2").value.trim().toUpperCase();
+  const gameCount = document.getElementById("game").value;
+  const setCount = document.getElementById("set").value;
+
+  // Verifica che i campi obbligatori siano compilati
+  if (!nameMatch || !nameP1 || !nameP2 || !setCount) {
+    alert("Per favore, completa tutti i campi obbligatori.");
+    return;
+  }
+
+  // Salva i dati nel localStorage
+  localStorage.setItem(
+    "matchSettings",
+    JSON.stringify({
+      nameMatch,
+      nameP1,
+      nameP2,
+      gameCount: parseInt(gameCount, 10),
+      setCount: parseInt(setCount, 10),
+    })
+  );
+
+  // Reindirizza alla pagina match.html
+  window.location.href = "match.html";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("gameInProgress") === "true") {
+    window.location.href = "match.html"; // Cambia con la tua pagina della partita
+  }
+});
