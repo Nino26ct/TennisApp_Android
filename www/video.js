a515419a0a0c89ca1e06c517e5f276562f75c930
Antonino Ferrarotti <antoninoferrarotti26@gmail.com>
2025-02-27 10:17:59 +0100

    sistemato un po il css della pagina e della sezione video

--- www/video.js
+++ www/video.js
@@ -269,8 +269,8 @@ function addVideoToPage(blob, id, matchState, matchSettings) {
     matchInfo.classList.add("match-info");
     matchInfo.setAttribute("data-id", id);
     matchInfo.innerHTML = `
-       <span>${nameP1} - <span class="scoreDisplayPlayer1">${scoreDisplayPlayer1}</span></span><br>
-  <span>${nameP2} - <span class="scoreDisplayPlayer2">${scoreDisplayPlayer2}</span></span>
+      <span>${nameP1} - <span class="scoreDisplayPlayer1">${scoreDisplayPlayer1}</span></span> <span class="vs">VS</span>
+      <span>${nameP2} - <span class="scoreDisplayPlayer2">${scoreDisplayPlayer2}</span></span>
     `;
 
     // Aggiungi icona video alle info
